446405facfbc5246d48426be9eaa155c3ff90335
Maia Dutta <mermaia33@gmail.com>
2023-03-15 22:56:57 -0500

    Registration and login working

--- weighttrackingapi/views/auth.py
+++ weighttrackingapi/views/auth.py
@@ -25,14 +25,20 @@ def login_user(request):
     # Use the built-in authenticate method to verify
     # authenticate returns the user object or None if no user is found
     authenticated_user = authenticate(username=username, password=password)
+    print(authenticated_user)
 
     # If authentication was successful, respond with their token
     if authenticated_user is not None:
         token = Token.objects.get(user=authenticated_user)
-
+        name = authenticated_user.first_name
+        user_id = authenticated_user.id
+        print("Authenticated ID:", user_id)
+        role = Employee.objects.get(user_id=user_id).role
         data = {
             'valid': True,
             'token': token.key,
+            'name': name,
+            'role': role
         }
         return Response(data)
     else:
@@ -51,7 +57,7 @@ def register_user(request):
     '''
     # Create a new user by invoking the `create_user` helper method
     # on Django's built-in User model
-
+    print("HELLO!!!!!!!!")
     missing_fields = []
     username = request.data.get('username', None)
     password = request.data.get('password', None)
