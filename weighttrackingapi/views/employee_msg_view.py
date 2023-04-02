4c8bab5263731592a4186aa7329a8570a466c770
Maia Dutta <mermaia33@gmail.com>
2023-04-02 12:53:27 -0500

    route created to count unread messages

--- weighttrackingapi/views/employee_msg_view.py
+++ weighttrackingapi/views/employee_msg_view.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from weighttrackingapi.models import EmployeeMessage, Employee
+from rest_framework.decorators import action
 from django.contrib.auth.models import User
 
 
@@ -47,6 +48,21 @@ class EmployeeMessageView(ViewSet):
         employee_msg = EmployeeMessage.objects.get(pk=pk)
         employee_msg.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    @action(detail=False, methods=['get'])
+    def unreadmessages(self, request, pk=None):
+        """Returns the number of unread messages for a recipient"""
+        employee_msgs = EmployeeMessage.objects.all()
+        # select the employee by their userid
+        employee = Employee.objects.get(user=request.query_params['recipient'])
+
+        if "recipient" in request.query_params:
+            employee_msgs = employee_msgs.filter(
+                recipient=employee, message__read=False)
+        res = {"num_msgs": len(employee_msgs)}
+        return Response(res)
+
+
 
 
 class EmployeeMessageSerializer(serializers.ModelSerializer):
